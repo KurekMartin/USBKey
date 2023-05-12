7bcc03bab34eef12e7c60e825b6aa8ba32ef421f
Martin Kurek <mar.kurek@seznam.cz>
2023-05-12 16:08:51 +0200

    usb detection

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using Usb.Events;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using IUsbEventWatcher usbEventWatcher = new UsbEventWatcher();
+
+        usbEventWatcher.UsbDriveMounted += (_, path) =>
+        {
+            Console.WriteLine(path);
+        };
+        Console.WriteLine("Insert USB");
+        while (true) { };
+    }
+}
