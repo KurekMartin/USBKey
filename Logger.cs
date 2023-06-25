bd2a95f697b8d85edd437e43ec42158b04805bcc
Martin Kurek <mar.kurek@seznam.cz>
2023-06-25 15:15:23 +0200

    log type color

--- Logger.cs
+++ Logger.cs
@@ -5,10 +5,25 @@
         Info, Warning, Error
     }
     internal static class Logger
-    {        
+    {
         public static void Log(LogType type, string message)
         {
-            Console.WriteLine($"[{type.ToString().ToUpper()}] {message}");
+            var color = Console.ForegroundColor;
+            switch (type)
+            {
+                case LogType.Info:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case LogType.Warning:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case LogType.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+            }
+            Console.Write($"[{type.ToString().ToUpper()}] ");
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
         }
     }
 }
