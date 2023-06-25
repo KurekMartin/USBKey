7e3f8306cd0a709fa9159b34956164d69eb5395e
Martin Kurek <mar.kurek@seznam.cz>
2023-06-25 18:27:24 +0200

    added processing of stages

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using Usb.Events;
 
 namespace USBKey
 {
     class Program
     {
+        private static readonly WaitingElement _waitingElement = new();
+        private static Random _random = new();
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
@@ -13,14 +16,14 @@ namespace USBKey
             Settings.Load();
 
             Console.Write("Vložte USB klíč ");
-            WaitingElement waitingElement = new(150);
+            _waitingElement.Show();
 
             using IUsbEventWatcher usbEventWatcher = new UsbEventWatcher();
             Data? data = null;
             usbEventWatcher.UsbDriveMounted += (_, path) =>
             {
                 data = USB.GetDataFromDrive(path);
-                waitingElement.Stop();
+                _waitingElement.Stop();
                 MainLoop(data);
             };
 
@@ -29,14 +32,58 @@ namespace USBKey
 
         static void MainLoop(Data? data)
         {
-            Random random = new();
+
+            foreach (var stage in Settings.Stages)
+            {
+                switch (stage.Type)
+                {
+                    case StageType.Message:
+                        ProcessMessage(stage);
+                        break;
+                    case StageType.Progress:
+                        ProcessProgress(stage);
+                        break;
+                }
+            }
+
+
             ProgressBar progressBar = new(30, "SampleTask");
             do
             {
-                progressBar.Value += random.Next(10);
-                Thread.Sleep(random.Next(1000));
+                progressBar.Value += _random.Next(10);
+                Thread.Sleep(_random.Next(1000));
             } while (progressBar.Value != 100);
             ProcessData(data);
+        }
+
+        static void ProcessMessage(Stage stage)
+        {
+            Console.WriteLine(stage.Text);
+            Thread.Sleep(stage.Duration);
+        }
+
+        static void ProcessProgress(Stage stage)
+        {
+            ProgressBar progressBar = new(stage.ProgressBarLength, stage.Text);
+            int totalDuration = 0;
+            do
+            {
+                if (totalDuration < stage.Duration)
+                {
+                    var durationVariance = _random.Next(-stage.StepDurationVariance, stage.StepDurationVariance);
+                    var duration = Math.Max(stage.StepDuration + durationVariance, 0);
+                    var step = _random.Next(1, stage.MaxStep);
+
+                    progressBar.Value += step;
+                    Thread.Sleep(duration);
+
+                    totalDuration += duration;
+                }
+                else
+                {
+                    progressBar.Value = 100;
+                }
+            } while (progressBar.Value != 100);
         }
 
         static void ProcessData(Data? data)
